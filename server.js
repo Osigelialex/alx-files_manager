1c67a54d432c095590f81355e8cecd87126f6aa1
mazi-kunle <olakunlemichael644@gmail.com>
2023-09-01 23:44:38 +0100

    complete task 3

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ import routes from './routes/index.js';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
+app.use(express.json());
 app.use('/', routes);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
